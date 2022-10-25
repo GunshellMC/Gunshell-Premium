d1148d061c967b5b127cfb54953715f227c63744
Esmaybe <esmaybe@revenity.net>
2022-10-26 00:14:28 +0200

    kutjazz

---
+++ src/main/java/com/jazzkuh/gunshell/common/actions/melee/MeleeDamageAction.java
@@ -0,0 +1,57 @@
+package com.jazzkuh.gunshell.common.actions.melee;
+
+import com.jazzkuh.gunshell.GunshellPlugin;
+import com.jazzkuh.gunshell.api.objects.GunshellMelee;
+import com.jazzkuh.gunshell.common.actions.melee.abstraction.AbstractMeleeAction;
+import com.jazzkuh.gunshell.compatibility.CompatibilityManager;
+import com.jazzkuh.gunshell.utils.PluginUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class MeleeDamageAction extends AbstractMeleeAction {
+    public MeleeDamageAction(GunshellMelee melee) {
+        super(melee);
+    }
+
+    @Override
+    public void fireAction(LivingEntity entity, Player player, ConfigurationSection configuration) {
+        if (entity.isDead()) return;
+        if (entity.hasMetadata("NPC")) return;
+
+        CompatibilityManager compatibilityManager = GunshellPlugin.getInstance().getCompatibilityManager();
+        if (entity instanceof Player) {
+            Player playerTarget = (Player) entity;
+            if (playerTarget.getGameMode() == GameMode.SPECTATOR
+                    || playerTarget.getGameMode() == GameMode.CREATIVE) return;
+
+            if (compatibilityManager.isExtensionEnabled(CompatibilityManager.Extension.COMBATTAGPLUS)) {
+                compatibilityManager.getCombatTagPlusExtension().getTagManager().tag(playerTarget, player);
+            }
+        }
+
+        if (entity.getLocation().getWorld() != null) {
+            entity.getLocation().getWorld().playEffect(entity.getEyeLocation(),
+                    Effect.STEP_SOUND, Material.REDSTONE_BLOCK);
+        }
+
+        double damage = this.getMelee().getDamage();
+        damage = PluginUtils.getInstance().applyProtectionModifier(damage, false, entity);
+
+        if (damage > entity.getHealth()) {
+            entity.setHealth(0D);
+        } else {
+            EntityDamageByEntityEvent entityDamageByEntityEvent = new EntityDamageByEntityEvent(player, entity,
+                    EntityDamageByEntityEvent.DamageCause.ENTITY_ATTACK, damage);
+            entity.setHealth(entity.getHealth() - damage);
+
+            entity.setLastDamageCause(entityDamageByEntityEvent);
+            Bukkit.getPluginManager().callEvent(entityDamageByEntityEvent);
+        }
+    }
+}
---
+++ src/main/java/com/jazzkuh/gunshell/common/listeners/ProjectileHitListener.java
@@ -0,0 +1,38 @@
+package com.jazzkuh.gunshell.common.listeners;
+
+import com.jazzkuh.gunshell.GunshellPlugin;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftSnowball;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProjectileHitListener implements Listener {
+    @EventHandler
+    public void onProjectileHit(final ProjectileHitEvent event) {
+        if (event.getEntity() instanceof Snowball && event.getEntity().hasMetadata("gunshell_grenade_bounce")) {
+            Snowball grenade = (Snowball) event.getEntity();
+            int count = grenade.getMetadata("gunshell_grenade_bounce").get(0).asInt();
+            if (count - 1 <= 0) return;
+
+            Snowball bouncyGrenade = grenade.getWorld().spawn(grenade.getLocation(), Snowball.class);
+            ((CraftSnowball) bouncyGrenade).getHandle()
+                    .a(CraftItemStack.asNMSCopy(new ItemStack(Material.STICK)), 1.0F);
+            bouncyGrenade.setMetadata("gunshell_grenade_bounce",
+                    new FixedMetadataValue(GunshellPlugin.getInstance(), count-1));
+
+            bouncyGrenade.setVelocity(new Vector(
+                    ThreadLocalRandom.current().nextDouble(-0.3D, 0.3D),
+                    ThreadLocalRandom.current().nextDouble(0.0D, 0.5D),
+                    ThreadLocalRandom.current().nextDouble(-0.3D, 0.3D)
+            ));
+        }
+    }
+}
