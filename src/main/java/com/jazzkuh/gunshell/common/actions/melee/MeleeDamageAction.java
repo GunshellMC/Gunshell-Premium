789490be802b0b29b1d4c94a7496ab10ed1818bf
Amberful <amberful@craftville.net>
2022-12-11 21:54:14 +0100

    Added to melee too

--- src/main/java/com/jazzkuh/gunshell/common/actions/melee/MeleeDamageAction.java
+++ src/main/java/com/jazzkuh/gunshell/common/actions/melee/MeleeDamageAction.java
@@ -1,5 +1,6 @@
 package com.jazzkuh.gunshell.common.actions.melee;
 
+import com.cryptomorin.xseries.XPotion;
 import com.jazzkuh.gunshell.GunshellPlugin;
 import com.jazzkuh.gunshell.api.objects.GunshellMelee;
 import com.jazzkuh.gunshell.common.actions.melee.abstraction.AbstractMeleeAction;
@@ -13,6 +14,9 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.List;
 
 public class MeleeDamageAction extends AbstractMeleeAction {
     public MeleeDamageAction(GunshellMelee melee) {
@@ -40,7 +44,25 @@ public class MeleeDamageAction extends AbstractMeleeAction {
                     Effect.STEP_SOUND, Material.REDSTONE_BLOCK);
         }
 
-        double damage = this.getMelee().getDamage();
+
+
+        double damage = 0;
+        List<String> actions = this.getMelee().getActions();
+        for (String action : actions) {
+            if (action.toUpperCase().startsWith("DAMAGE")) {
+                damage += Integer.parseInt(action.split(":")[1]);
+            } else if (action.toUpperCase().startsWith("POTION")) {
+                String[] potionInfo = action.split(":");
+                XPotion.matchXPotion(potionInfo[1]).ifPresent(potion -> {
+                    int level = Integer.parseInt(potionInfo[2]);
+                    int duration = Integer.parseInt(potionInfo[3]);
+
+                    if (potion.getPotionEffectType() == null) return;
+                    player.addPotionEffect(new PotionEffect(potion.getPotionEffectType(), duration, level));
+                });
+            }
+        }
+
         damage = PluginUtils.getInstance().applyProtectionModifier(damage, false, entity);
 
         if (damage > entity.getHealth()) {
